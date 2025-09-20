9485a2866d91bdc0124f50c084160d51877fc782
Roy Hashimoto <roy@shoestringresearch.com>
2025-09-20 16:33:34 -0700

    Use AsyncEntityProducers.createBinary().

--- src/main/kotlin/MotionJPEGHandler.kt
+++ src/main/kotlin/MotionJPEGHandler.kt
@@ -4,8 +4,7 @@ import org.apache.hc.core5.http.message.BasicNameValuePair
 import org.apache.hc.core5.http.nio.AsyncEntityConsumer
 import org.apache.hc.core5.http.nio.AsyncRequestConsumer
 import org.apache.hc.core5.http.nio.AsyncServerRequestHandler
-import org.apache.hc.core5.http.nio.StreamChannel
-import org.apache.hc.core5.http.nio.entity.AbstractBinAsyncEntityProducer
+import org.apache.hc.core5.http.nio.entity.AsyncEntityProducers
 import org.apache.hc.core5.http.nio.entity.DiscardingEntityConsumer
 import org.apache.hc.core5.http.nio.support.AsyncResponseBuilder
 import org.apache.hc.core5.http.nio.support.BasicRequestConsumer
@@ -24,16 +23,18 @@ private val JPEG_FILES = arrayListOf(
 private val logger = Logger.getLogger("MotionJPEGHandler")
 
 // Handler that returns a motion JPEG sequence using
-// MIME type multipart/x-mixed-replace. This is an
-// infinite sequence so the response does not end.
-//
-// The interesting code is actually in the producer
-// class below this one.
+// MIME type multipart/x-mixed-replace.
 //
 // This handler works on HTTP but throws a
 // H2StreamResetException on HTTPS.
 class MotionJPEGHandler(
 ) : AsyncServerRequestHandler<Message<HttpRequest, Void>> {
+  private val boundary = "o3L1JqT8W9yR4cK7pX0vF2zA"
+  private val contentType = ContentType.create(
+    "multipart/x-mixed-replace",
+    BasicNameValuePair("boundary", boundary)
+  )
+
   override fun prepare(
     request: HttpRequest,
     entityDetails: EntityDetails?,
@@ -52,7 +53,26 @@ class MotionJPEGHandler(
     logger.info("handle: ${requestObject.head}")
 
     // Send JPEG images to the requesting client.
-    val entity = MotionJPEGEntityProducer()
+    var index = 0
+    val bufferQueue = ArrayDeque<ByteBuffer>()
+    val entity = AsyncEntityProducers.createBinary({ stream ->
+      if (bufferQueue.isEmpty()) {
+        if (index < JPEG_FILES.size) {
+          // Enqueue the buffers for the next image.
+          addBuffers(bufferQueue, JPEG_FILES[index++])
+        } else {
+          stream.endStream()
+          return@createBinary
+        }
+      }
+
+      val buffer = bufferQueue.first()
+      stream.write(buffer)
+      if (!buffer.hasRemaining()) {
+        bufferQueue.removeFirst()
+      }
+    }, contentType)
+
     val response = AsyncResponseBuilder.create(HttpStatus.SC_OK)
       .setHeader(BasicHeader(HttpHeaders.CACHE_CONTROL, "no-cache"))
       .setHeader(BasicHeader(HttpHeaders.PRAGMA, "no-cache"))
@@ -60,57 +80,24 @@ class MotionJPEGHandler(
       .build()
     responseTrigger.submitResponse(response, context)
   }
-}
 
-// This producer sends an infinite sequence of multipart
-// JPEG images.
-private class MotionJPEGEntityProducer(
-  fragmentSizeHint: Int = 65536
-) : AbstractBinAsyncEntityProducer(
-  fragmentSizeHint,
-  ContentType.create(
-    "multipart/x-mixed-replace",
-    BasicNameValuePair("boundary", BOUNDARY))) {
-  companion object {
-    private const val BOUNDARY = "o3L1JqT8W9yR4cK7pX0vF2zA"
-  }
-
-  private var index = 0
-  private var bufferQueue = ArrayDeque<ByteBuffer>()
-
-  override fun availableData() = Int.MAX_VALUE
-  override fun isRepeatable() = false
-
-  override fun produceData(channel: StreamChannel<ByteBuffer>) {
-    // Block until a buffer is available to write.
-    if (bufferQueue.isEmpty()) {
-      val filename = JPEG_FILES[index++ % JPEG_FILES.size]
-      val jpegImage = object {}::class.java.getResourceAsStream(filename)!!.use {
-        ByteBuffer.wrap(it.readBytes())
-      }
-
-      val header = StringBuilder()
-        .append("--$BOUNDARY\r\n")
-        .append("${HttpHeaders.CONTENT_TYPE}: image/jpeg\r\n")
-        .append("${HttpHeaders.CONTENT_LENGTH}: ${jpegImage.remaining()}\r\n")
-        .append("\r\n")
-      val hBuffer = ByteBuffer.wrap(header.toString().toByteArray())
-      bufferQueue.addLast(hBuffer)
-      bufferQueue.addLast(jpegImage)
+  fun addBuffers(
+    bufferQueue: ArrayDeque<ByteBuffer>,
+    filename: String
+  ) {
+    val imageBuffer = object {}::class.java.getResourceAsStream(filename)!!.use {
+      ByteBuffer.wrap(it.readBytes())
     }
 
-    val byteBuffer = bufferQueue.first()
-    val nWritten = channel.write(byteBuffer)
-    logger.info("produceData: $nWritten written")
+    // Build multipart header.
+    val header = StringBuilder()
+      .append("--$boundary\r\n")
+      .append("${HttpHeaders.CONTENT_TYPE}: image/jpeg\r\n")
+      .append("${HttpHeaders.CONTENT_LENGTH}: ${imageBuffer.remaining()}\r\n")
+      .append("\r\n")
+    val headerBuffer = ByteBuffer.wrap(header.toString().toByteArray())
 
-    if (!byteBuffer.hasRemaining()) {
-      bufferQueue.removeFirst()
-    } else {
-      logger.info("produceData: ${byteBuffer.remaining()} remaining")
-    }
-  }
-
-  override fun failed(cause: java.lang.Exception) {
-    // Server will log exception.
+    bufferQueue.addLast(headerBuffer)
+    bufferQueue.addLast(imageBuffer)
   }
 }
