7c7c5f467171c9a69dd300cb7698bb0a39b1d53e
Roy Hashimoto <roy@shoestringresearch.com>
2025-09-23 10:46:44 -0700

    HTTP1 with H2ServerTlsStrategy.

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+  kotlin("jvm") version "2.2.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.apache.httpcomponents.core5:httpcore5-h2:5.3.5")
+  implementation("org.conscrypt:conscrypt-openjdk-uber:2.5.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+  testImplementation(kotlin("test"))
+}
+
+tasks.jar.configure {
+  manifest {
+    attributes(mapOf("Main-Class" to "MainKt"))
+  }
+  configurations["compileClasspath"].forEach { file ->
+    from(zipTree(file.absoluteFile))
+  }
+  duplicatesStrategy = DuplicatesStrategy.INCLUDE
+  exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
+kotlin {
+  jvmToolchain(24)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,98 @@
+import org.apache.hc.core5.http.*
+import org.apache.hc.core5.http.nio.AsyncEntityConsumer
+import org.apache.hc.core5.http.nio.AsyncRequestConsumer
+import org.apache.hc.core5.http.nio.AsyncServerRequestHandler
+import org.apache.hc.core5.http.nio.entity.AsyncEntityProducers
+import org.apache.hc.core5.http.nio.entity.DiscardingEntityConsumer
+import org.apache.hc.core5.http.nio.support.AsyncResponseBuilder
+import org.apache.hc.core5.http.nio.support.BasicRequestConsumer
+import org.apache.hc.core5.http.protocol.HttpContext
+import org.apache.hc.core5.http2.HttpVersionPolicy
+import org.apache.hc.core5.http2.config.H2Config
+import org.apache.hc.core5.http2.impl.nio.bootstrap.H2ServerBootstrap
+import org.apache.hc.core5.http2.ssl.H2ServerTlsStrategy
+import org.apache.hc.core5.io.CloseMode
+import org.apache.hc.core5.reactor.IOReactorConfig
+import org.apache.hc.core5.util.TimeValue
+import org.conscrypt.Conscrypt
+import java.net.InetSocketAddress
+import java.security.KeyStore
+import java.util.logging.Logger
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+
+private val logger = Logger.getLogger("WebServer")
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  val port = 8443
+  val sslContext = buildSSLContext(
+    "/keyStore.p12",
+    "password".toCharArray()
+  )
+
+ val server = H2ServerBootstrap.bootstrap()
+    .setCanonicalHostName("localhost")
+    .setH2Config(H2Config.DEFAULT)
+    .setIOReactorConfig(IOReactorConfig.DEFAULT)
+    .setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_1)
+    .setExceptionCallback {
+      logger.info(it.stackTraceToString())
+    }
+    .setTlsStrategy(H2ServerTlsStrategy(sslContext))
+    .register("*", CustomServerRequestHandler())
+    .create()
+
+  server.start()
+  val endpoint = server.listen(
+    InetSocketAddress(port),
+    URIScheme.HTTPS)
+    .get()
+  logger.info("Listening on ${endpoint.address}")
+
+  Runtime.getRuntime().addShutdownHook(Thread() {
+    server.close(CloseMode.GRACEFUL)
+  })
+  server.awaitShutdown(TimeValue.ofDays(Long.MAX_VALUE))
+}
+
+private fun buildSSLContext(
+  keyStorePath: String,
+  keyStorePassword: CharArray
+): SSLContext {
+  val keyStore = KeyStore.getInstance("PKCS12")
+  object {}::class.java.getResourceAsStream(keyStorePath).use {
+    keyStore.load(it, keyStorePassword)
+  }
+
+  return SSLContext.getInstance("TLSv1.2", Conscrypt.newProvider()).apply {
+    val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+    keyManagerFactory.init(keyStore, keyStorePassword)
+    init(keyManagerFactory.keyManagers, null, null)
+  }
+}
+
+private class CustomServerRequestHandler : AsyncServerRequestHandler<Message<HttpRequest, Void>> {
+  override fun prepare(
+    request: HttpRequest, entityDetails: EntityDetails?, context: HttpContext
+  ): AsyncRequestConsumer<Message<HttpRequest, Void>> {
+    val dataConsumer: AsyncEntityConsumer<Void>? = if (entityDetails != null) DiscardingEntityConsumer() else null
+    return BasicRequestConsumer(dataConsumer)
+  }
+
+  override fun handle(
+    requestObject: Message<HttpRequest, Void>,
+    responseTrigger: AsyncServerRequestHandler.ResponseTrigger,
+    context: HttpContext
+  ) {
+    logger.info("handle $context")
+    val response = AsyncResponseBuilder.create(HttpStatus.SC_OK)
+      .setEntity(
+        AsyncEntityProducers.create(
+          "Hello, World!\n", ContentType.TEXT_PLAIN
+        )
+      ).build()
+    responseTrigger.submitResponse(response, context)
+  }
+}
