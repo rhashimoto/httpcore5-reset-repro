fc3cfecc3ecacb070173932231593f266f0150a3
Roy Hashimoto <roy@shoestringresearch.com>
2025-09-26 13:49:18 -0700

    Allow HTTP2.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
+import org.apache.hc.core5.function.Supplier
 import org.apache.hc.core5.http.*
+import org.apache.hc.core5.http.impl.routing.RequestRouter
 import org.apache.hc.core5.http.nio.AsyncEntityConsumer
 import org.apache.hc.core5.http.nio.AsyncRequestConsumer
+import org.apache.hc.core5.http.nio.AsyncServerExchangeHandler
 import org.apache.hc.core5.http.nio.AsyncServerRequestHandler
 import org.apache.hc.core5.http.nio.entity.AsyncEntityProducers
 import org.apache.hc.core5.http.nio.entity.DiscardingEntityConsumer
 import org.apache.hc.core5.http.nio.support.AsyncResponseBuilder
 import org.apache.hc.core5.http.nio.support.BasicRequestConsumer
+import org.apache.hc.core5.http.nio.support.BasicServerExchangeHandler
 import org.apache.hc.core5.http.protocol.HttpContext
+import org.apache.hc.core5.http.protocol.HttpProcessorBuilder
 import org.apache.hc.core5.http2.HttpVersionPolicy
 import org.apache.hc.core5.http2.config.H2Config
 import org.apache.hc.core5.http2.impl.nio.bootstrap.H2ServerBootstrap
+import org.apache.hc.core5.http2.protocol.H2RequestConformance
+import org.apache.hc.core5.http2.protocol.H2RequestConnControl
+import org.apache.hc.core5.http2.protocol.H2RequestContent
 import org.apache.hc.core5.http2.ssl.ConscryptServerTlsStrategy
 import org.apache.hc.core5.io.CloseMode
 import org.apache.hc.core5.reactor.IOReactorConfig
@@ -32,23 +40,33 @@ fun main() {
     "password".toCharArray()
   )
 
- val server = H2ServerBootstrap.bootstrap()
+  val server = H2ServerBootstrap.bootstrap()
     .setCanonicalHostName("localhost")
-    .setH2Config(H2Config.DEFAULT)
     .setIOReactorConfig(IOReactorConfig.DEFAULT)
-    .setVersionPolicy(HttpVersionPolicy.FORCE_HTTP_1)
+    .setH2Config(H2Config.DEFAULT)
+    .setHttpProcessor(HttpProcessorBuilder.create()
+      .add(H2RequestConformance.INSTANCE)
+      .add(H2RequestConnControl.INSTANCE)
+      .add(H2RequestContent.INSTANCE)
+      .build())
     .setExceptionCallback {
       logger.info(it.stackTraceToString())
     }
     .setTlsStrategy(ConscryptServerTlsStrategy(sslContext))
-    .register("*", CustomServerRequestHandler())
+    .setRequestRouter(
+      RequestRouter.builder<Supplier<AsyncServerExchangeHandler>>()
+        .resolveAuthority(RequestRouter.LOCAL_AUTHORITY_RESOLVER)
+        .addRoute(RequestRouter.LOCAL_AUTHORITY, "*") {
+          BasicServerExchangeHandler(CustomServerRequestHandler())
+        }.build())
     .create()
 
   server.start()
   val endpoint = server.listen(
     InetSocketAddress(port),
-    URIScheme.HTTPS)
-    .get()
+    URIScheme.HTTPS,
+    HttpVersionPolicy.NEGOTIATE,
+    null).get()
   logger.info("Listening on ${endpoint.address}")
 
   Runtime.getRuntime().addShutdownHook(Thread() {
